c263c615587f2bae463988e79996213dbbf5a1c8
Paweł Szumański <szumapman@outlook.com>
2019-01-22 09:52:59 +0100

    updated bootstrap

--- src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
@@ -4,6 +4,7 @@ import guru.springframework.spring5webfluxrest.domain.Category;
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ public class Bootstrap implements CommandLineRunner {
     private final CategoryRepository categoryRepository;
     private final VendorRepository vendorRepository;
 
+    @Autowired
     public Bootstrap(CategoryRepository categoryRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.vendorRepository = vendorRepository;
