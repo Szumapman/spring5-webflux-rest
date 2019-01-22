ddcacd7b41e42eb5c9c4c4d7130b58c3a22f7c0a
Paweł Szumański <szumapman@outlook.com>
2019-01-22 10:35:02 +0100

    updated bootstrap

--- src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
@@ -1,26 +1,24 @@
 package guru.springframework.spring5webfluxrest.bootstrap;
 
-import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
-import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
-    private final CategoryRepository categoryRepository;
-    private final VendorRepository vendorRepository;
-
-    public Bootstrap(CategoryRepository categoryRepository, VendorRepository vendorRepository) {
-        this.categoryRepository = categoryRepository;
-        this.vendorRepository = vendorRepository;
-    }
+//    private final CategoryRepository categoryRepository;
+//    private final VendorRepository vendorRepository;
+//
+//    public Bootstrap(CategoryRepository categoryRepository, VendorRepository vendorRepository) {
+//        this.categoryRepository = categoryRepository;
+//        this.vendorRepository = vendorRepository;
+//    }
 
     @Override
     public void run(String... args) throws Exception {
-            if(categoryRepository.count().block() == 0){
-                //load data
-                System.out.println("###### LOADING DATA ON BOOTSTRAP ######");
+//            if(categoryRepository.count().block() == 0){
+//                //load data
+//                System.out.println("###### LOADING DATA ON BOOTSTRAP ######");
 
 //                categoryRepository.save(Category.builder().description("Fruits").build()).block();
 //                categoryRepository.save(Category.builder().description("Nuts").build()).block();
@@ -37,7 +35,7 @@ public class Bootstrap implements CommandLineRunner {
 //                vendorRepository.save(Vendor.builder().firstName("Paul").lastName("Szuman").build()).block();
 //
 //                System.out.println("Loaded Vendors: " + vendorRepository.count().block());
-
-            }
+//
+//            }
     }
 }
