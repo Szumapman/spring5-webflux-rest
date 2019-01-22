26ad2f12f1d70b17574236c4a3aad1948c6d166f
Paweł Szumański <szumapman@outlook.com>
2019-01-22 10:32:06 +0100

    updated bootstrap

--- src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/spring5webfluxrest/bootstrap/Bootstrap.java
@@ -1,12 +1,11 @@
 package guru.springframework.spring5webfluxrest.bootstrap;
 
-import guru.springframework.spring5webfluxrest.domain.Category;
-import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
@@ -23,21 +22,21 @@ public class Bootstrap implements CommandLineRunner {
                 //load data
                 System.out.println("###### LOADING DATA ON BOOTSTRAP ######");
 
-                categoryRepository.save(Category.builder().description("Fruits").build()).block();
-                categoryRepository.save(Category.builder().description("Nuts").build()).block();
-                categoryRepository.save(Category.builder().description("Breads").build()).block();
-                categoryRepository.save(Category.builder().description("Meats").build()).block();
-                categoryRepository.save(Category.builder().description("Eggs").build()).block();
-
-                System.out.println("Loaded Categories: " + categoryRepository.count().block());
-
-                vendorRepository.save(Vendor.builder().firstName("Joe").lastName("Black").build()).block();
-                vendorRepository.save(Vendor.builder().firstName("Michale").lastName("Black").build()).block();
-                vendorRepository.save(Vendor.builder().firstName("Jessie").lastName("James").build()).block();
-                vendorRepository.save(Vendor.builder().firstName("Bill").lastName("Gates").build()).block();
-                vendorRepository.save(Vendor.builder().firstName("Paul").lastName("Szuman").build()).block();
-
-                System.out.println("Loaded Vendors: " + vendorRepository.count().block());
+//                categoryRepository.save(Category.builder().description("Fruits").build()).block();
+//                categoryRepository.save(Category.builder().description("Nuts").build()).block();
+//                categoryRepository.save(Category.builder().description("Breads").build()).block();
+//                categoryRepository.save(Category.builder().description("Meats").build()).block();
+//                categoryRepository.save(Category.builder().description("Eggs").build()).block();
+//
+//                System.out.println("Loaded Categories: " + categoryRepository.count().block());
+//
+//                vendorRepository.save(Vendor.builder().firstName("Joe").lastName("Black").build()).block();
+//                vendorRepository.save(Vendor.builder().firstName("Michale").lastName("Black").build()).block();
+//                vendorRepository.save(Vendor.builder().firstName("Jessie").lastName("James").build()).block();
+//                vendorRepository.save(Vendor.builder().firstName("Bill").lastName("Gates").build()).block();
+//                vendorRepository.save(Vendor.builder().firstName("Paul").lastName("Szuman").build()).block();
+//
+//                System.out.println("Loaded Vendors: " + vendorRepository.count().block());
 
             }
     }
