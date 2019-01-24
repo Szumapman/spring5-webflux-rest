975411c3d39c0086dffe78d8496d1a7b428fb460
Paweł Szumański <szumapman@outlook.com>
2019-01-24 16:22:30 +0100

    test in circleCi

--- src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -20,29 +20,4 @@ public class Vendor {
     private String firstName;
     private String lastName;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
 }
